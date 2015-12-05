c5cf6fb54917d40eccf6237d3c16f888ba7cbfbc
Adam Alton <adamalton@gmail.com>
2015-12-05 23:07:11 +0000

    Making the Django server run on 0.0.0.0 so that it accepts incoming connections from other devices.

--- ssh/start_server.sh
+++ ssh/start_server.sh
@@ -2,4 +2,6 @@
 cd ~/photoframe_env
 source bin/activate
 cd floto-flame
-nohup python manage.py runserver &
+# By specifying 0.0.0.0 we allow it to accept incoming network connections from other devices
+# on the same network, i.e. it allows me to access /shutdown/ from my phone on my home wifi
+nohup python manage.py runserver 0.0.0.0:8000 &
