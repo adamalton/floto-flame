6b9bf7f386a08359c3eced033f87552e55a1a77d
Adam Alton <adam@potatolondon.com>
2016-09-04 12:45:22 +0100

    Update to Django 1.10.  Fiddle settings & urlpatterns to make it work.
    I want the `.set()` method on ManyToManyField’s related manager.

--- floto/urls.py
+++ floto/urls.py
@@ -1,14 +1,18 @@
-from django.conf.urls import patterns, include, url
+# THIRD PARTY
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('floto.views',
+# FLOTO
+from floto import views
 
+
+urlpatterns = [
     # url(r'^$', 'home', name='home'),
-    url(r'^frame/$', 'frame', name='frame'),
-    url(r'^get-photo-list/$', 'get_photo_list', name='get_photo_list'),
-    url(r'^trigger-photo-list-refresh/$', 'trigger_photo_list_refresh', name='trigger_photo_list_refresh'),
-    url(r'^image-proxy/(?P<photo_id>\d+)/$', 'image_proxy', name='image_proxy'),
-    url(r'^start-oauth/$', 'start_oauth', name='start_oauth'),
-    url(r'^oauth-callback/$', 'oauth_callback', name='oauth_callback'),
-    url(r'shutdown/$', 'shutdown', name='shutdown'),
-)
+    url(r'^frame/$', views.frame, name='frame'),
+    url(r'^get-photo-list/$', views.get_photo_list, name='get_photo_list'),
+    url(r'^trigger-photo-list-refresh/$', views.trigger_photo_list_refresh, name='trigger_photo_list_refresh'),
+    url(r'^image-proxy/(?P<photo_id>\d+)/$', views.image_proxy, name='image_proxy'),
+    url(r'^start-oauth/$', views.start_oauth, name='start_oauth'),
+    url(r'^oauth-callback/$', views.oauth_callback, name='oauth_callback'),
+    url(r'shutdown/$', views.shutdown, name='shutdown'),
+]
