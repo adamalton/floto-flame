d870f90e51673f5a617ac159322ca930cfdbfbbf
Adam Alton <adamalton@gmail.com>
2016-09-05 07:08:58 +0000

    Run server with sudo because... erm... I changed it to do that, there must have been a reason.

--- ssh/start_server.sh
+++ ssh/start_server.sh
@@ -4,4 +4,5 @@ source bin/activate
 cd floto-flame
 # By specifying 0.0.0.0 we allow it to accept incoming network connections from other devices
 # on the same network, i.e. it allows me to access /shutdown/ from my phone on my home wifi
-nohup python manage.py runserver 0.0.0.0:8000 &
+#sudo nohup python manage.py runserver 0.0.0.0:8000 &
+sudo nohup python manage.py runserver 0.0.0.0:8000 &
